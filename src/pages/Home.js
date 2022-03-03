ed7337209a595d50065635d46b578c42677bb0b8
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-03-03 17:47:21 +0900

    ♻️ Refactor : data home으로 이동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,17 @@ import styled from "styled-components";
 import Options from "../components/Options";
 import Settings from "../components/Settings";
 import Selectors from "../components/Selectors";
+import { useState } from "react";
+import { emojiMenus } from "../components/emojiMenus";
+
 const Home = () => {
+  const [dataOne, setDataOne] = useState({
+    1: [...emojiMenus.slice(0, 2)],
+    2: [...emojiMenus.slice(3, 4)],
+  });
   return (
     <MainContainer>
-      <Options />
+      <Options dataOne={dataOne} setDataOne={setDataOne} />
       <Selectors />
       <Settings />
     </MainContainer>
