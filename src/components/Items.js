3dad442cab19c9af2ebfe572047fd3f0b820bf1a
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-03-04 19:43:21 +0900

    ✨ Feat : ctrl, shift 다중 선택 기능 구현

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useDragAndDrop } from "../hooks/useDragAndDrop";
 import { useRecoilState } from "recoil";
-import { fontSizeAtom } from "../atom";
+import { fontSizeAtom, leftTitleAtom } from "../atom";
 
 const Optionsli = styled.li`
   cursor: pointer;
@@ -14,6 +14,9 @@ const Optionsli = styled.li`
   align-items: center;
   height: 25px;
   line-height: 14px;
+  &:hover {
+    background-color: #b5d8f0;
+  }
 `;
 const Emoji = styled.div`
   margin-right: 5px;
@@ -37,18 +40,46 @@ const Items = ({
   });
 
   const [fontSize] = useRecoilState(fontSizeAtom);
-  const selectItem = (id) => {
+  const [firstClick, setFirstClick] = useState(0);
+
+  const selectItem = (e, id, idx) => {
     setDirection(direction);
+
+    if (e.shiftKey) {
+      let copyData = Object.assign([], list);
+      let select = [];
+      const start = firstClick < idx ? firstClick : idx;
+      const end = firstClick < idx ? idx + 1 : firstClick + 1;
+
+      copyData.splice(start, end).map((item) => select.push(item.id));
+
+      setSelectId(select);
+      return;
+    }
+
+    if (e.ctrlKey) {
+      setFirstClick(idx);
+      setSelectId((prev) => [...prev, id]);
+      return;
+    }
+
     // 선택된게 하나도 없으면 추가
     if (selectId.length === 0) {
+      setFirstClick(idx);
       setSelectId([id]);
       return;
     }
 
-    if (selectId.findIndex((item) => item === id)) {
-      setSelectId([id]);
+    if (selectId.findIndex((item) => item === id) !== -1) {
+      let data = [];
+      setFirstClick(0);
+      if (selectId.length !== 1) {
+        data = (prev) => prev.filter((element) => element !== id);
+      }
+      setSelectId(data);
     } else {
-      setSelectId([]);
+      setFirstClick(idx);
+      setSelectId([id]);
     }
   };
 
@@ -64,10 +95,10 @@ const Items = ({
           style={{
             backgroundColor:
               selectId.findIndex((id) => id === item.id) === -1
-                ? "white"
+                ? ""
                 : "#b5d8f0",
           }}
-          onClick={() => selectItem(item.id)}
+          onClick={(e) => selectItem(e, item.id, idx)}
           onDragOver={(e) => e.preventDefault()}
           onDragEnter={(e) => onDragEnter(e, idx)}
         >
